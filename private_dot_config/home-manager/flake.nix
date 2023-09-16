bbe8a0ea28e78e03a3d65fcf12a27a651812c824
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-09-16 11:18:55 -0300

    removed hyprland module

--- private_dot_config/home-manager/flake.nix
+++ private_dot_config/home-manager/flake.nix
@@ -8,15 +8,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-		hyprland.url = "github:hyprwm/Hyprland";
-		split-monitor-workspaces = {
-			url = "github:Duckonaut/split-monitor-workspaces";
-			inputs.hyprland.follows = "hyprland";
-		};
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, split-monitor-workspaces, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -33,8 +27,6 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
-					hyprland.homeManagerModules.default
-					{wayland.windowManager.hyprland.enable = true;}
 					./home.nix
 				];
         # Optionally use extraSpecialArgs
