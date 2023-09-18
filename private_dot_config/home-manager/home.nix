ab2e47b81d863702ea381a71b6b24f2d5d4b3a05
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-09-18 10:00:39 -0300

    removed comments

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -1,44 +1,17 @@
 { config, pkgs, inputs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "joaoqueiroga";
   home.homeDirectory = "/home/joaoqueiroga";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.stateVersion = "23.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
 		discord
 		teams-for-linux
 		drawio
 		obsidian
 		inputs.st.packages.${pkgs.system}.st
-    /* (st.overrideAttrs (oldAttrs: rec {
-      buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
-      src = builtins.fetchTarball {
-				url = "https://gitlab.com/Joao-Queiroga/st/-/archive/main/st-main.tar.gz";
-      };
-    })) */
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
 	gtk = {
@@ -53,18 +26,5 @@
 		};
 	};
 	
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/joaoqueiroga/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
