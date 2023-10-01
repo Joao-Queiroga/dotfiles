4a601c832070c5fa7b7731f84d554a06bb14141d
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-01 17:04:48 -0300

    changed lf config to add previews

--- private_dot_config/home-manager/lf.nix
+++ private_dot_config/home-manager/lf.nix
@@ -3,7 +3,16 @@
 	programs.lf = {
 		enable = true;
 		commands = {
-			dragon = ''%''$${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
+			open = ''
+				''${{
+					test -L $f && f=$(readlink -f $f)
+					case $(file --mime-type $f -b) in
+						text/*|application/json) $EDITOR "$f";;
+						*) xdg-open "$f";;
+					esac
+				}}
+			'';
+			dragon = ''& ${pkgs.xdragon}/bin/dragon -a -x "$fx"'';
 			edit = ''$$EDITOR $f'';
 			mkdir = ''
 			''${{
@@ -20,6 +29,10 @@
 			icons = true;
 			ignorecase = true;
 		};
+		keybindings = {
+			"." = "set hidden!";
+			V = ''''$${pkgs.bat}/bin/bat --paging=always "$f"'';
+		};
 		extraConfig =
 		let
 			previewer = pkgs.writeShellScriptBin "pv.sh" ''
@@ -29,12 +42,25 @@
 				x=$4
 				y=$5
 
-				if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-					${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+				image() {
+					${pkgs.kitty}/bin/kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$1" < /dev/null > /dev/tty
 					exit 1
-				fi
+				}
 
-				${pkgs.pistol}/bin/pistol "$file"
+				case "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" in
+					image/*) image "$file";;
+					video/*)
+						CACHE="''${XDG_CACHE_HOME:-$HOME/.cache}/lf/thumb.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$file")" | sha256sum | cut -d' ' -f1)"
+						[ ! -f "$CACHE" ] && ${pkgs.ffmpegthumbnailer}/bin/ffmpegthumbnailer -i "$file" -o "$CACHE" -s 0
+						image "$CACHE"
+						;;
+					application/pdf)
+							CACHE="''${XDG_CACHE_HOME:-$HOME/.cache}/lf/thumb.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | cut -d' ' -f1)"
+							[ ! -f "$CACHE.jpg" ] && ${pkgs.poppler_utils}/bin/pdftoppm -jpeg -f 1 -singlefile "$1" "$CACHE"
+							image "$CACHE.jpg" "$2" "$3" "$4" "$5" "$1"
+							;;
+					*) ${pkgs.pistol}/bin/pistol "$file";;
+				esac
 			'';
 			cleaner = pkgs.writeShellScriptBin "clean.sh" ''
 				${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
