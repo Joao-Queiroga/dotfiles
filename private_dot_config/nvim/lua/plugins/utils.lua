bffe65dedd0206c68ea4ec4ea3bc0ba21bd5d9e9
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-05 10:22:09 -0300

    refactored and reformated

--- private_dot_config/nvim/lua/plugins/utils.lua
+++ private_dot_config/nvim/lua/plugins/utils.lua
@@ -1,54 +1,52 @@
 return {
 	{
-		'numToStr/Navigator.nvim',
+		"numToStr/Navigator.nvim",
 		event = "VeryLazy",
 		opts = {},
 	},
 	{
-		'uga-rosa/ccc.nvim',
+		"uga-rosa/ccc.nvim",
 		opts = {
 			highlighter = {
 				auto_enable = true,
 				lsp = true,
-			}
-		}
+			},
+		},
 	},
 	{
-		'echasnovski/mini.align',
+		"echasnovski/mini.align",
 		keys = {
-			"ga",
-			"gA",
-			{"ga", mode = "v"},
-			{"gA", mode = "v"},
+			{ "ga", mode = { "n", "v" } },
+			{ "gA", mode = { "n", "v" } },
 		},
-		version = '*',
+		version = "*",
 		opts = {
 			mappings = {
-				start_with_preview = 'ga',
-				start = 'gA',
-			}
-		}
+				start_with_preview = "ga",
+				start = "gA",
+			},
+		},
 	},
 	{
-		'akinsho/toggleterm.nvim',
-		version = '*',
-		keys = {{"<C-t>", mode = { "i", "n" }}},
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		keys = { { "<C-t>", mode = { "i", "n" } } },
 		opts = {
 			size = 14,
 			open_mapping = [[<c-t>]],
 		},
 	},
 	{
-		'dhruvasagar/vim-table-mode',
-		event = "VeryLazy"
+		"dhruvasagar/vim-table-mode",
+		event = "VeryLazy",
 	},
 	{
-		'lambdalisue/suda.vim',
-		init = function ()
+		"lambdalisue/suda.vim",
+		init = function()
 			vim.g.suda_smart_edit = 1
-		end
+		end,
 	},
 	{
-		'gentoo/gentoo-syntax'
-	}
+		"gentoo/gentoo-syntax",
+	},
 }
