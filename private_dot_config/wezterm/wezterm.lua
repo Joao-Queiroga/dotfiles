166ff9d0f63c420fa2ba605bd8a21113fc6d4974
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-11 08:58:10 -0300

    reformated

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,15 +1,16 @@
-local wezterm = require 'wezterm'
-local keys = require 'keys'
+local wezterm = require("wezterm")
+local keys = require("keys")
 local config = {}
-config.font = wezterm.font 'JetBrains Mono'
+config.term = "wezterm"
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 12
 
-config.color_scheme = 'tokyonight_night'
+config.color_scheme = "tokyonight_night"
 config.tab_and_split_indices_are_zero_based = true
 
-config.window_close_confirmation = 'NeverPrompt'
+config.window_close_confirmation = "NeverPrompt"
 
-config.leader = { key = "b", mods="CTRL" }
+config.leader = { key = "b", mods = "CTRL" }
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
@@ -17,15 +18,15 @@ config.tab_bar_at_bottom = true
 config.window_padding = {
 	bottom = 0,
 	left = 0,
-	right = 0
+	right = 0,
 }
 
 config.keys = keys
 
 config.unix_domains = {
 	{
-		name = 'unix'
-	}
+		name = "unix",
+	},
 }
 
 return config
