003380cee83971971bd6ebd8102139fd13104429
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-14 10:20:10 -0300

    added crates.nvim plugin

--- private_dot_config/nvim/lua/plugins/lsp/init.lua
+++ private_dot_config/nvim/lua/plugins/lsp/init.lua
@@ -1,41 +1,41 @@
-local handlers = require'plugins.lsp.handlers'
+local handlers = require("plugins.lsp.handlers")
 local installed_servers = {
-	'lua_ls',
-	'rust_analyzer',
-	'clangd',
-	'gopls',
-	'jdtls',
-	'vimls',
-	'jsonls',
-	'yamlls',
-	'rnix',
-	'nil_ls'
+	"lua_ls",
+	"rust_analyzer",
+	"clangd",
+	"gopls",
+	"jdtls",
+	"vimls",
+	"jsonls",
+	"yamlls",
+	"rnix",
+	"nil_ls",
 }
 return {
 	{
-		'neovim/nvim-lspconfig',
-		config = handlers.setup
+		"neovim/nvim-lspconfig",
+		config = handlers.setup,
 	},
 	{
-		'williamboman/mason-lspconfig.nvim',
+		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
-			'williamboman/mason.nvim',
-			'folke/neoconf.nvim',
-			'neovim/nvim-lspconfig',
+			"williamboman/mason.nvim",
+			"folke/neoconf.nvim",
+			"neovim/nvim-lspconfig",
 		},
 		opts = {
 			ensure_installed = installed_servers,
-			handlers = { handlers.choose_handler }
-		}
+			handlers = { handlers.choose_handler },
+		},
 	},
 	{
-		'folke/neoconf.nvim',
+		"folke/neoconf.nvim",
 		opts = {},
 	},
 	{
 		"smjonas/inc-rename.nvim",
 		cmd = "IncRename",
-		config = true
+		config = true,
 	},
 	{
 		"folke/trouble.nvim",
@@ -44,32 +44,60 @@ return {
 		opts = {},
 	},
 	{
-		'nvimdev/lspsaga.nvim',
+		"nvimdev/lspsaga.nvim",
 		event = "LspAttach",
 		dependencies = {
-			'nvim-treesitter/nvim-treesitter',
-			'nvim-tree/nvim-web-devicons'
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
 		},
 		opts = {
 			outline = {
-				layout = 'float',
+				layout = "float",
 				keys = {
-					toggle_or_jump = '<Tab>',
-					jump = 'l',
-				}
-			}
-		}
+					toggle_or_jump = "<Tab>",
+					jump = "l",
+				},
+			},
+		},
 	},
 	{
-		'folke/neodev.nvim',
-		lazy = true
+		"folke/neodev.nvim",
+		lazy = true,
 	},
 	{
-		'mfussenegger/nvim-jdtls',
-		lazy = true
+		"mfussenegger/nvim-jdtls",
+		lazy = true,
 	},
 	{
-		'simrat39/rust-tools.nvim',
-		lazy = true
+		"simrat39/rust-tools.nvim",
+		lazy = true,
+	},
+	{
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("BufRead", {
+				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+				pattern = "Cargo.toml",
+				callback = function()
+					require("cmp").setup.buffer({ sources = { { name = "crates" } } })
+				end,
+			})
+		end,
+		opts = {
+			src = {
+				cmp = {
+					enabled = true,
+				},
+			},
+			null_ls = {
+				enabled = true,
+				name = "crates.nvim",
+			},
+		},
 	},
 }
