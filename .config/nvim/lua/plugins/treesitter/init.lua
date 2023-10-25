85427aeabd9865a72faec37dc912349e57d744f9
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-25 07:57:44 -0300

    changed events to load

--- .config/nvim/lua/plugins/treesitter/init.lua
+++ .config/nvim/lua/plugins/treesitter/init.lua
@@ -1,24 +1,24 @@
 return {
 	{
-		'nvim-treesitter/nvim-treesitter',
-		event="VeryLazy",
+		"nvim-treesitter/nvim-treesitter",
+		event =  { "BufRead", "BufNewFile", "VeryLazy" },
 		dependencies = {
-			'nvim-treesitter/playground',
-			'hiphish/rainbow-delimiters.nvim',
-			'JoosepAlviste/nvim-ts-context-commentstring',
-			'nvim-treesitter/nvim-treesitter-refactor',
+			"nvim-treesitter/playground",
+			"hiphish/rainbow-delimiters.nvim",
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"nvim-treesitter/nvim-treesitter-refactor",
 			"luckasRanarison/tree-sitter-hypr",
 		},
 		build = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
-		config = function ()
-			require'plugins.treesitter.config'
-		end
+		config = function()
+			require("plugins.treesitter.config")
+		end,
 	},
 	{
-		'windwp/nvim-ts-autotag',
+		"windwp/nvim-ts-autotag",
 		event = "InsertEnter",
 		dependencies = "nvim-treesitter/nvim-treesitter",
 		opts = {},
