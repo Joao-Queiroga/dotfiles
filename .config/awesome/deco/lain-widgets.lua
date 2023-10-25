ca31b4db294f492d17bb13e185dca611084ac782
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-25 15:53:31 -0300

    added error handling for a nil value

--- .config/awesome/deco/lain-widgets.lua
+++ .config/awesome/deco/lain-widgets.lua
@@ -1,5 +1,5 @@
 --Wibar widgets
-local lain  = require("lain")
+local lain = require("lain")
 local markup = lain.util.markup
 local beautiful = require("beautiful")
 
@@ -9,22 +9,22 @@ local font = "JetbrainsMono Nerd Font 11"
 widgets.cpu = lain.widget.cpu({
 	settings = function()
 		widget:set_markup(markup.fontfg(font, "#ff5555", "  " .. cpu_now.usage .. "%"))
-	end
+	end,
 })
 
 widgets.memory = lain.widget.mem({
 	timeout = 1,
 	settings = function()
 		widget:set_markup(markup.fontfg(font, "#f1fa8c", "󰍛 " .. mem_now.used .. "M"))
-	end
+	end,
 })
 
 widgets.volume = lain.widget.alsa({
 	timeout = 2,
 	settings = function()
 		local volume_icon = volume_now.status == "on" and "󰕾 " or "󰝟 "
-		widget:set_markup(markup.fontfg(font, "#50fa7b", volume_icon .. volume_now.level .. "%"))
-	end
+		widget:set_markup(markup.fontfg(font, "#50fa7b", volume_icon .. (volume_now.level or "") .. "%"))
+	end,
 })
 
 return widgets
