27aca9fac84585363ee7a6493f3614d93b0bbe2a
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-10-31 09:19:14 -0300

    added a map to put a bar at each monitor

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,6 +1,7 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
+import { exec } from 'resource:///com/github/Aylur/ags/utils.js';
 import Bar from './bar.js'
 
 const exampleWindow = Widget.Window({
@@ -9,6 +10,9 @@ const exampleWindow = Widget.Window({
 		label: 'example-content',
 	}),
 });
+
+// had to use exec since Hyprland.monitors was returning a empty array
+const monitors = JSON.parse(exec('hyprctl monitors -j'))
 
 export default {
 	closeWindowDelay: {
@@ -22,6 +26,6 @@ export default {
 		// NOTE: the window will still render, if you don't pass it here
 		// but if you don't, the window can't be toggled through App or cli
 		exampleWindow,
-		Bar({ monitor: 0 }),
+		...monitors.map((_, i) => Bar({ monitor: i })),
 	],
 };
