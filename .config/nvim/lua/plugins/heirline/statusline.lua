4cfb3108e1c1ad588d5997ff14d97d94bec0c195
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-02 08:09:44 -0300

    added ruler and scrollbar to heirline

--- .config/nvim/lua/plugins/heirline/statusline.lua
+++ .config/nvim/lua/plugins/heirline/statusline.lua
@@ -203,10 +203,34 @@ local diagnostics = {
 	},
 }
 
+local Ruler = {
+	-- %l = current line number
+	-- %L = number of lines in the buffer
+	-- %c = column number
+	-- %P = percentage through file of displayed window
+	provider = "%7(%l/%3L%):%2c %P",
+}
+
+local ScrollBar = {
+	static = {
+		sbar = { "🭶", "🭷", "🭸", "🭹", "🭺", "🭻" },
+	},
+	provider = function(self)
+		local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+		local lines = vim.api.nvim_buf_line_count(0)
+		local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+		return string.rep(self.sbar[i], 2)
+	end,
+	hl = { fg = "blue", bg = "bg_highlight" },
+}
+
 return {
 	utils.surround({ "", "" }, "bg_highlight", { vi_mode }),
 	diagnostics,
 	file_name_block,
+	{ provider = "%=" },
+	Ruler,
+	ScrollBar,
 
 	hl = { bg = "bg_dark" },
 	condition = exlude,
