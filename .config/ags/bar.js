b8ecb6f9a2f1cde32d47f7e569caebd74ec3890e
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-09 19:12:19 -0300

    reformated

--- .config/ags/bar.js
+++ .config/ags/bar.js
@@ -1,63 +1,87 @@
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
-import { execAsync } from 'resource:///com/github/Aylur/ags/utils.js';
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import Hyprland from "resource:///com/github/Aylur/ags/service/hyprland.js";
+import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 
-const Workspaces = (monitor) => Widget.Box({
-	className: 'workspaces',
-	connections:  [[Hyprland.active.workspace, self => {
-		// generate an array [1..9] then make buttons from the index
-		const arr = Array.from({ length: 9 }, (_, i) => i + 1);
-		self.children = arr.map(i => Widget.Button({
-			onClicked: () => execAsync(`hyprctl dispatch workspace ${i + (monitor * 10)}`),
-			child: Widget.Label(`${i}`),
-			className: Hyprland.active.workspace.id == i + (monitor * 10) ? 'focused' : '',
-		}))
-	}]],
-})
+const Workspaces = (monitor) =>
+  Widget.Box({
+    className: "workspaces",
+    connections: [
+      [
+        Hyprland.active.workspace,
+        (self) => {
+          // generate an array [1..9] then make buttons from the index
+          const arr = Array.from({ length: 9 }, (_, i) => i + 1);
+          self.children = arr.map((i) =>
+            Widget.Button({
+              onClicked: () =>
+                execAsync(`hyprctl dispatch workspace ${i + monitor * 10}`),
+              child: Widget.Label(`${i}`),
+              className:
+                Hyprland.active.workspace.id == i + monitor * 10
+                  ? "focused"
+                  : "",
+            }),
+          );
+        },
+      ],
+    ],
+  });
 
-const ClientTitle = (monitor) => Widget.Label({
-	className: 'client-title',
-	connections: [[Hyprland.active, self => {
-		let mon = Hyprland.getMonitor(monitor)
-		self.label = mon.focused? Hyprland.active.client.title : Hyprland.getWorkspace(mon.activeWorkspace.id).lastwindowtitle
-	}]]
-})
+const ClientTitle = (monitor) =>
+  Widget.Label({
+    className: "client-title",
+    connections: [
+      [
+        Hyprland.active,
+        (self) => {
+          let mon = Hyprland.getMonitor(monitor);
+          self.label = mon.focused
+            ? Hyprland.active.client.title
+            : Hyprland.getWorkspace(mon.activeWorkspace.id).lastwindowtitle;
+        },
+      ],
+    ],
+  });
 
-const Clock = () => Widget.Label({
-	className: 'clock',
-	connections: [
-		[1000, self => execAsync(['date', '+%a %d/%m/%Y %H:%M'])
-			.then(date => self.label = date).catch(console.error)]
-	]
-})
+const Clock = () =>
+  Widget.Label({
+    className: "clock",
+    connections: [
+      [
+        1000,
+        (self) =>
+          execAsync(["date", "+%a %d/%m/%Y %H:%M"])
+            .then((date) => (self.label = date))
+            .catch(console.error),
+      ],
+    ],
+  });
 
-const Left = (monitor) => Widget.Box({
-	children: [
-		Workspaces(monitor),
-	]
-})
+const Left = (monitor) =>
+  Widget.Box({
+    children: [Workspaces(monitor)],
+  });
 
-const Center = (monitor) => Widget.Box({
-	children: [
-		ClientTitle(monitor)
-	]
-})
+const Center = (monitor) =>
+  Widget.Box({
+    children: [ClientTitle(monitor)],
+  });
 
-const Right = (monitor) => Widget.Box({
-	children: [
-		Clock()
-	]
-})
+const Right = (monitor) =>
+  Widget.Box({
+    children: [Clock()],
+  });
 
-export default ({ monitor } = {}) => Widget.Window({
-	name: `bar-${monitor}`,
-	className: 'bar',
-	monitor,
-	exclusive: true,
-	anchor: ['top', 'left', 'right'],
-	child: Widget.CenterBox({
-		startWidget: Left(monitor),
-		centerWidget: Center(monitor),
-		endWidget: Right(),
-	})
-})
+export default ({ monitor } = {}) =>
+  Widget.Window({
+    name: `bar-${monitor}`,
+    className: "bar",
+    monitor,
+    exclusive: true,
+    anchor: ["top", "left", "right"],
+    child: Widget.CenterBox({
+      startWidget: Left(monitor),
+      centerWidget: Center(monitor),
+      endWidget: Right(),
+    }),
+  });
--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,21 +1,21 @@
-import App from 'resource:///com/github/Aylur/ags/app.js';
-import { exec } from 'resource:///com/github/Aylur/ags/utils.js';
-import Bar from './bar.js'
+import App from "resource:///com/github/Aylur/ags/app.js";
+import { exec } from "resource:///com/github/Aylur/ags/utils.js";
+import Bar from "./bar.js";
 
 // had to use exec since Hyprland.monitors was returning a empty array
-const monitors = JSON.parse(exec('hyprctl monitors -j'))
+const monitors = JSON.parse(exec("hyprctl monitors -j"));
 
 export default {
-	closeWindowDelay: {
-		'window-name': 500, // milliseconds
-	},
-	notificationPopupTimeout: 5000, // milliseconds
-	cacheNotificationActions: false,
-	maxStreamVolume: 1.5, // float
-	style: App.configDir + '/style.css' ,
-	windows: [
-		// NOTE: the window will still render, if you don't pass it here
-		// but if you don't, the window can't be toggled through App or cli
-		...monitors.map((monitor) => Bar({ monitor: monitor.id })),
-	],
+  closeWindowDelay: {
+    "window-name": 500, // milliseconds
+  },
+  notificationPopupTimeout: 5000, // milliseconds
+  cacheNotificationActions: false,
+  maxStreamVolume: 1.5, // float
+  style: App.configDir + "/style.css",
+  windows: [
+    // NOTE: the window will still render, if you don't pass it here
+    // but if you don't, the window can't be toggled through App or cli
+    ...monitors.map((monitor) => Bar({ monitor: monitor.id })),
+  ],
 };
