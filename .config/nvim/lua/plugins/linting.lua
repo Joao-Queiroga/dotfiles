e7cc6920c9daea2dbd815cfe482afdb62f6c6515
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-09 19:11:37 -0300

    configured lua linting and corrected nvim-lint error

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -1,20 +1,22 @@
 return {
 	{
 		"mfussenegger/nvim-lint",
-		event = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		event = { "BufWrite", "BufRead", "InsertLeave" },
 		opts = {
 			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 			linters_by_ft = {
 				fish = { "fish" },
 				python = { "flake8" },
+				lua = { "selene" },
 			},
 		},
 		config = function(_, opts)
 			local lint = require("lint")
 			lint.linters_by_ft = opts.linters_by_ft
 			vim.api.nvim_create_autocmd(opts.events, {
-				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-				callback = lint.try_lint,
+				callback = function()
+					lint.try_lint()
+				end,
 			})
 		end,
 	},
