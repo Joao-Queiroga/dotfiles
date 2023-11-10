2feb7fab8e55484d563ce76aba5a7833353c11f7
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-10 14:19:05 -0300

    added mini.surround

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -5,6 +5,30 @@ return {
 		config = true,
 	},
 	{
+		"echasnovski/mini.surround",
+		version = false,
+		keys = {
+			{ "gsa", desc = "Add surrounding", mode = { "n", "v" } },
+			{ "gsd", desc = "Delete surrounding", mode = { "n", "v" } },
+			{ "gsf", desc = "Find surrounding (to the right)" },
+			{ "gsF", desc = "Find surrounding (to the left)" },
+			{ "gsh", desc = "Highlight surrounding", mode = { "n", "v" } },
+			{ "gsr", desc = "Replace surrounding", mode = { "n", "v" } },
+			{ "gsn", desc = "Update `n_lines`", mode = { "n", "v" } },
+		},
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		},
+	},
+	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = function()
