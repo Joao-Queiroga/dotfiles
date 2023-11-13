9e4c2c88f60e7b661bc0db5e7ba88c6b5f7bac70
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-13 11:21:01 -0300

    changed to scss in ags

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -5,6 +5,11 @@ import Bar from "./bar.js";
 // had to use exec since Hyprland.monitors was returning a empty array
 const monitors = JSON.parse(exec("hyprctl monitors -j"));
 
+const scss = App.configDir + "/style.scss";
+const css = App.configDir + "/style.css";
+
+exec(`sassc ${scss} ${css}`);
+
 export default {
   closeWindowDelay: {
     "window-name": 500, // milliseconds
@@ -12,7 +17,7 @@ export default {
   notificationPopupTimeout: 5000, // milliseconds
   cacheNotificationActions: false,
   maxStreamVolume: 1.5, // float
-  style: App.configDir + "/style.css",
+  style: css,
   windows: [
     // NOTE: the window will still render, if you don't pass it here
     // but if you don't, the window can't be toggled through App or cli
