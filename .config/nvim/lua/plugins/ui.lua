bf83436643987de05285ffd872172ad7152ee17b
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-17 15:27:34 -0300

    changed noice.nvim config

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -8,7 +8,18 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
-			presets = { inc_rename = true },
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+			},
 		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
