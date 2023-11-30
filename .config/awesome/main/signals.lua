304b6edf9b0632f9bc812ef4eefeaeee149b344f
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-11-30 11:03:25 -0300

    added signal to unmaximize maximized windows

--- .config/awesome/main/signals.lua
+++ .config/awesome/main/signals.lua
@@ -9,19 +9,23 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 
 -- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
+client.connect_signal("manage", function(c)
 	-- Set the windows at the slave,
 	-- i.e. put it at the end of others instead of setting it master.
 	-- if not awesome.startup then awful.client.setslave(c) end
 
-	c.shape = function (cr, w, h)
-		gears.shape.rounded_rect(cr, w, h, 5)	
+	c.shape = function(cr, w, h)
+		gears.shape.rounded_rect(cr, w, h, 5)
 	end
-	if awesome.startup
-	  and not c.size_hints.user_position
-	  and not c.size_hints.program_position then
+	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
 		-- Prevent clients from being unreachable after screen count changes.
 		awful.placement.no_offscreen(c)
+	end
+end)
+
+client.connect_signal("property::maximized", function(c)
+	if c.maximized then
+		c.maximized = false
 	end
 end)
 
@@ -30,8 +34,12 @@ require("deco.titlebar")
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
-	c:emit_signal("request::activate", "mouse_enter", {raise = false})
+	c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c)
+	c.border_color = beautiful.border_focus
+end)
+client.connect_signal("unfocus", function(c)
+	c.border_color = beautiful.border_normal
+end)
