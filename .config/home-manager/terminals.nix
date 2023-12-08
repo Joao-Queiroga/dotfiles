55ee4915a32a847434de8efa2e8b40209e4d69cb
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2023-12-08 20:16:37 -0300

    added alacritty config

--- .config/home-manager/terminals.nix
+++ .config/home-manager/terminals.nix
@@ -65,4 +65,49 @@
     };
     theme = "Tokyo Night";
   };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal = {
+          family = "JetBrains Mono Nerd Font";
+          size = 13;
+        };
+      };
+      colors = {
+        primary = {
+          background = "#1a1b26";
+          foreground = "#c0caf5";
+        };
+        cursor = {
+          text = "CellBackground";
+          cursor = "CellForeground";
+        };
+        normal = {
+          black = "#15161e";
+          red = "#f7768e";
+          green = "#9ece6a";
+          yellow = "#e0af68";
+          blue = "#7aa2f7";
+          magenta = "#bb9af7";
+          cyan = "#7dcfff";
+          white = "#a9b1d6";
+        };
+        bright = {
+          black = "#414868";
+          red = "#f7768e";
+          green = "#9ece6a";
+          yellow = "#e0af68";
+          blue = "#7aa2f7";
+          magenta = "#bb9af7";
+          cyan = "#7dcfff";
+          white = "#c0caf5";
+        };
+        indexed_colors = [
+          { index = 16; color = "#ff9e64"; }
+          { index = 17; color = "#db4b4b"; }
+        ];
+      };
+    };
+  };
 }
