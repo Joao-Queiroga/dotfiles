f5766d8ce92bd02d7f33535f6857347a9d9fae08
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-01-13 22:36:17 -0300

    improved method for generating a bar per monitor
    took form aylur config

--- .config/ags/js/main.js
+++ .config/ags/js/main.js
@@ -1,14 +1,8 @@
-import { exec, execAsync } from "resource:///com/github/Aylur/ags/utils.js";
+import { forMonitors } from "./utils.js";
 import Bar from "./bar/bar.js";
 
-// had to use exec since Hyprland.monitors was returning a empty array
-const monitors = execAsync(["hyprctl", "monitors", "-j"])
-  .then((res) => JSON.parse(res))
-  .catch((_) => undefined);
+const Windows = [forMonitors(Bar)];
 
 export default {
-  closeWindowDelay: {
-    "window-name": 500, // milliseconds
-  },
-  windows: [...(await monitors)?.map((monitor) => Bar(monitor.id))],
+  windows: Windows,
 };
--- .config/ags/js/utils.js
+++ .config/ags/js/utils.js
@@ -1,3 +1,5 @@
+import Gdk from "gi://Gdk";
+
 /**
  * @param {number} length
  * @param {number=} start
@@ -5,4 +7,13 @@
  */
 export function range(length, start = 1) {
   return Array.from({ length }, (_, i) => i + start);
+}
+
+/**
+ * @param {(monitor: number) => any} widget
+ * @returns {Array<import('types/widgets/window').default>}
+ */
+export function forMonitors(widget) {
+  const n = Gdk.Display.get_default()?.get_n_monitors() || 1;
+  return range(n, 0).map(widget).flat(1);
 }
