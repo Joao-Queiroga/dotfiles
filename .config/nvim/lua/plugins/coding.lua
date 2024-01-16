1f6c2326a3f0aed039f68a8aa44af53833f67938
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-01-16 08:31:46 -0300

    changed project.nvim config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -101,8 +101,16 @@ return {
 		dependencies = {
 			"nvim-telescope/telescope.nvim",
 		},
-		config = function()
-			require("project_nvim").setup()
+		opts = {
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+		},
+		config = function(_, opts)
+			require("project_nvim").setup(opts)
 			require("telescope").load_extension("projects")
 		end,
 	},
