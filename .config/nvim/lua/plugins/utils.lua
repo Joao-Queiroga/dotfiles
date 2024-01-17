5219e3359d82004fc747815f6c30ccbcac875790
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-01-17 12:28:58 -0300

    added vim-startuptime plugin

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -68,6 +68,13 @@ return {
 		opts = {},
 	},
 	{
+		"dstein64/vim-startuptime",
+		cmd = "StartupTime",
+		config = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
+	{
 		"gentoo/gentoo-syntax",
 		event = "BufReadPre",
 	},
