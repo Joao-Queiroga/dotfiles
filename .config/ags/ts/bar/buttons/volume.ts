b2756548351e11bb6993698c34ff81835deeb054
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-01-19 20:45:51 -0300

    changed volume icon muted logic(mute the speaker instead of changing the volume to 0)

--- .config/ags/ts/bar/buttons/volume.ts
+++ .config/ags/ts/bar/buttons/volume.ts
@@ -4,9 +4,8 @@ import Audio from "resource:///com/github/Aylur/ags/service/audio.js";
 const volumeIndicator = () =>
   Widget.Button({
     on_clicked: () => {
-      if (Audio.speaker) {
-        Audio.speaker.is_muted = !Audio.speaker.is_muted;
-      }
+      if (Audio.speaker)
+        Audio.speaker.stream.change_is_muted(!Audio.speaker.stream.is_muted);
     },
     class_name: "volume-indicator",
 
@@ -14,18 +13,22 @@ const volumeIndicator = () =>
       Audio,
       (self) => {
         if (!Audio.speaker) return;
+        const speaker = Audio.speaker;
 
-        const vol = Audio.speaker.volume * 100;
-        const icon = [
-          [101, "overamplified"],
-          [67, "high"],
-          [34, "medium"],
-          [1, "low"],
-          [0, "muted"],
-        ].find(([threshold]) => Number(threshold) <= vol)?.[1];
+        const vol = speaker.volume * 100;
+        const icon = speaker.stream.isMuted
+          ? "muted"
+          : [
+              [101, "overamplified"],
+              [67, "high"],
+              [34, "medium"],
+              [1, "low"],
+            ].find(([threshold]) => Number(threshold) <= vol)?.[1];
 
         self.icon = `audio-volume-${icon}-symbolic`;
-        self.tooltipText = `Volume ${Math.floor(vol)}%`;
+        self.tooltipText = `Volume ${Math.floor(vol)}%${
+          speaker.stream.isMuted ? "M" : ""
+        }`;
       },
       "speaker-changed",
     ),
