5ba4b50609253b7428e754dc32a628b12556344f
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-01-29 21:28:12 -0300

    added vertical tabs to firefox using sidebery. Still have some problems that will be fixed in the future.

--- .config/home-manager/browser/firefox.nix
+++ .config/home-manager/browser/firefox.nix
@@ -1,19 +1,18 @@
-{ pkgs, system, inputs, ... }:
-{
+{ pkgs, system, inputs, ... }: {
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-bin;
     profiles.joaoq = {
       userChrome = builtins.readFile ./userChrome.css;
-      userContent = builtins.readFile ./userContent.css;
       settings = {
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
       extraConfig = builtins.readFile ./user.js;
-      extensions = with inputs.firefox-addons.packages.${system};[
+      extensions = with inputs.firefox-addons.packages.${system}; [
         ublock-origin
         darkreader
         surfingkeys
+        sidebery
         videospeed
         gopass-bridge
       ];
