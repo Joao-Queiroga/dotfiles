4d95bedf4249d36e035731c4840f0cd94edfdd0b
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-02-06 21:11:06 -0300

    changed autopairs plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -29,71 +29,10 @@ return {
 		},
 	},
 	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {
-			check_ts = true,
-			disable_filetype = { "TelescopePrompt", "spectre_panel" },
-			fast_wrap = {
-				map = "<M-e>",
-				chars = { "{", "[", "(", '"', "'" },
-				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-				offset = 0, -- Offset from pattern match
-				end_key = "$",
-				keys = "qwertyuiopzxcvbnmasdfghjkl",
-				check_comma = true,
-				highlight = "PmenuSel",
-				highlight_grey = "LineNr",
-			},
-		},
-		config = function(_, opts)
-			local npairs = require("nvim-autopairs")
-			local Rule = require("nvim-autopairs.rule")
-			npairs.setup(opts)
-			-- Add spaces between parentheses
-			local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
-			npairs.add_rules({
-				Rule(" ", " "):with_pair(function(opts)
-					local pair = opts.line:sub(opts.col - 1, opts.col)
-					return vim.tbl_contains({
-						brackets[1][1] .. brackets[1][2],
-						brackets[2][1] .. brackets[2][2],
-						brackets[3][1] .. brackets[3][2],
-					}, pair)
-				end),
-			})
-			for _, bracket in pairs(brackets) do
-				npairs.add_rules({
-					Rule(bracket[1] .. " ", " " .. bracket[2])
-						:with_pair(function()
-							return false
-						end)
-						:with_move(function(opts)
-							return opts.prev_char:match(".%" .. bracket[2]) ~= nil
-						end)
-						:use_key(bracket[2]),
-				})
-			end
-			-- Move past commas and semicolons
-			for _, punct in pairs({ ",", ";" }) do
-				npairs.add_rules({
-					Rule("", punct)
-						:with_move(function(opts)
-							return opts.char == punct
-						end)
-						:with_pair(function()
-							return false
-						end)
-						:with_del(function()
-							return false
-						end)
-						:with_cr(function()
-							return false
-						end)
-						:use_key(punct),
-				})
-			end
-		end,
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		branch = "v0.6", --recomended as each new version will have breaking changes
+		opts = {},
 	},
 	{
 		"ahmedkhalf/project.nvim",
