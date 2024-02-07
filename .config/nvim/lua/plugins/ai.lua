0145988b2b13f458f3f18918e04337242a619dd5
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-02-07 11:00:28 -0300

    move chatgpt keybindings definition to plugin declaration

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,6 +1,36 @@
 return {
 	{
 		"jackMort/ChatGPT.nvim",
+		keys = {
+			{ "<leader>Cc", ":ChatGPT<CR>", mode = { "n", "v", "x" }, desc = "ChatGPT" },
+			{
+				"<leader>Ce",
+				":ChatGPTEditWithInstruction<CR>",
+				mode = { "n", "v", "x" },
+				desc = "Edit with instruction",
+			},
+			{
+				"<leader>Cg",
+				":ChatGPTRun grammar_correction<CR>",
+				mode = { "n", "v", "x" },
+				desc = "Grammar Correction",
+			},
+			{ "<leader>Ct", ":ChatGPTRun translate<CR>", mode = { "n", "v", "x" }, desc = "Translate" },
+			{ "<leader>Ck", ":ChatGPTRun keywords<CR>", mode = { "n", "v", "x" }, desc = "Keywords" },
+			{ "<leader>Cd", ":ChatGPTRun docstring<CR>", mode = { "n", "v", "x" }, desc = "Docstring" },
+			{ "<leader>Ca", ":ChatGPTRun add_tests<CR>", mode = { "n", "v", "x" }, desc = "Add Tests" },
+			{ "<leader>Co", ":ChatGPTRun optimize_code<CR>", mode = { "n", "v", "x" }, desc = "Optimize Code" },
+			{ "<leader>Cs", ":ChatGPTRun summarize<CR>", mode = { "n", "v", "x" }, desc = "Summarize" },
+			{ "<leader>Cf", ":ChatGPTRun fix_bugs<CR>", mode = { "n", "v", "x" }, desc = "Fix Bugs" },
+			{ "<leader>Cx", ":ChatGPTRun explain_code<CR>", mode = { "n", "v", "x" }, desc = "Explain Code" },
+			{ "<leader>Cr", ":ChatGPTRun roxygen_edit<CR>", mode = { "n", "v", "x" }, desc = "Roxygen Edit" },
+			{
+				"<leader>Cl",
+				":ChatGPTRun code_readability_analysis<CR>",
+				mode = { "n", "v", "x" },
+				desc = "Code Readability Analysis",
+			},
+		},
 		cmd = {
 			"ChatGPT",
 			"ChatGPTActAs",
