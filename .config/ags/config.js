4fa4bf0d948ec088feed24119cc78dcc18e9b943
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-03-08 11:38:22 -0300

    correct import

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,35 +1,19 @@
-import App from "resource:///com/github/Aylur/ags/app.js";
-import { exec, execAsync } from "resource:///com/github/Aylur/ags/utils.js";
-
-const scss = App.configDir + "/style/style.scss";
-const css = "/tmp/ags/style.css";
-
-exec(`sassc ${scss} ${css}`);
-
 const entry = App.configDir + "/ts/main.ts";
 const outdir = "/tmp/ags/js";
 
 try {
-  print(
-    await execAsync([
-      "bun",
-      "build",
-      entry,
-      "--outdir",
-      outdir,
-      "--external",
-      "resource://*",
-      "--external",
-      "gi://*",
-    ]),
-  );
+  await Utils.execAsync([
+    "bun",
+    "build",
+    entry,
+    "--outdir",
+    outdir,
+    "--external",
+    "resource://*",
+    "--external",
+    "gi://*",
+  ]);
+  await import(`file://${outdir}/main.js`);
 } catch (error) {
   console.error(error);
 }
-
-const main = await import(`file://${outdir}/main.js`);
-
-export default {
-  ...main.default,
-  style: css,
-};
--- .config/ags/ts/main.ts
+++ .config/ags/ts/main.ts
@@ -1,12 +1,20 @@
 import { forMonitors } from "./utils.ts";
 import Bar from "./bar/bar.ts";
 import Dashboard from "./dashboard/dashboard";
+import { exec } from "resource:///com/github/Aylur/ags/utils.js";
+import App from "resource:///com/github/Aylur/ags/app.js";
+
+const scss = `${App.configDir}/style.scss`;
+const css = "/tmp/ags/style.css";
+
+exec(`sassc ${scss} ${css}`);
 
 const Windows = [
   forMonitors(Bar),
   // forMonitors(Dashboard)
 ];
 
-export default {
+App.config({
+  style: css,
   windows: Windows,
-};
+});
