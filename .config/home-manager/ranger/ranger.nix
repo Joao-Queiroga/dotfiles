1ef56545b70d343a5de8986c6b474b4b762af422
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-03-12 19:42:56 -0300

    add scope.sh and mapping for drag

--- .config/home-manager/ranger/ranger.nix
+++ .config/home-manager/ranger/ranger.nix
@@ -5,6 +5,10 @@
       preview_images = true;
       preview_images_method = "kitty";
     };
+    mappings = {
+      "<C-d>" = "shell ${pkgs.ripdrag}/bin/ripdrag -ax %p 2>/dev/null &";
+    };
   };
   home.file.".config/ranger/commands.py".source = ./commands.py;
+  home.file.".config/ranger/scope.sh".source = ./scope.sh;
 }
