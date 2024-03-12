d6a87f43309814c855079cda292c8c8eb8ec9471
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-03-12 20:07:19 -0300

    add function to use command in the background

--- .config/home-manager/ranger/ranger.nix
+++ .config/home-manager/ranger/ranger.nix
@@ -5,9 +5,14 @@
       preview_images = true;
       preview_images_method = "kitty";
     };
-    mappings = {
-      "<C-d>" = "shell ${pkgs.ripdrag}/bin/ripdrag -ax %p 2>/dev/null &";
+    aliases = let
+      bg_script = pkgs.writeShellScriptBin "bg.sh" "$@ >/dev/null &";
+      bg = commando: "shell ${bg_script}/bin/bg.sh ${commando}";
+    in {
+      drag = bg "${pkgs.ripdrag}/bin/ripdrag -ax %p";
+      extract = bg "${pkgs.atool}/bin/atool -x %p";
     };
+    mappings = { "<C-d>" = "drag"; };
   };
   home.file.".config/ranger/commands.py".source = ./commands.py;
   home.file.".config/ranger/scope.sh".source = ./scope.sh;
