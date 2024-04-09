1b1a30dd495785be58f8985a37668da196644a26
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-04-09 10:50:41 -0300

    yazi: add smart-enter

--- .config/home-manager/yazi/yazi.nix
+++ .config/home-manager/yazi/yazi.nix
@@ -6,4 +6,21 @@
     keymap = import ./keymap.nix { pkgs = pkgs; };
     theme = import ./theme.nix;
   };
+  home.file."yazi-lua" = {
+    enable = true;
+    source = ./init.lua;
+    target = ".config/yazi/init.lua";
+  };
+  home.file."yazi-plugins" = {
+    enable = true;
+    source = ./plugins;
+    target = ".config/yazi/plugins";
+    recursive = true;
+  };
+  home.file."yazi-flavors" = {
+    enable = true;
+    source = ./flavors;
+    target = ".config/yazi/flavors";
+    recursive = true;
+  };
 }
