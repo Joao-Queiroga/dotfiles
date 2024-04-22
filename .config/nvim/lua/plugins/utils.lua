439c66cd217bf6ab8c06715853974684467d92f0
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-04-22 11:47:28 -0300

    nvim: add lazy-loading to mini.move

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -35,6 +35,12 @@ return {
 	},
 	{
 		"echasnovski/mini.move",
+		keys = {
+			{ "<M-h>", mode = { "n", "v" } },
+			{ "<M-j>", mode = { "n", "v" } },
+			{ "<M-k>", mode = { "n", "v" } },
+			{ "<M-l>", mode = { "n", "v" } },
+		},
 		opts = {},
 	},
 	{
