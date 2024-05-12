bea74447e0bf12f97d382f582c34d191f9a63265
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-05-11 22:04:20 -0300

    nvim: change completion module to a single file
    a

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,153 @@
+local kind_icons = {
+	Text = "󰉿",
+	Copilot = "",
+	Codeium = "",
+	Method = "",
+	Function = "󰊕",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Array = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "󰎠",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+}
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+			"zbirenbaum/copilot-cmp",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local defaults = require("cmp.config.default")()
+			return {
+				mapping = {
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-J>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+					["<C-K>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+					["<C-e>"] = cmp.mapping({
+						i = cmp.mapping.abort(),
+						c = cmp.mapping.close(),
+					}),
+					-- Accept currently selected item. If none selected, `select` first item.
+					-- Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				},
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						-- Kind icons
+						vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+						vim_item.menu = ({
+							copilot = "[Copilot]",
+							nvim_lsp = "[LSP]",
+							nvim_lua = "[Nvim_Lua]",
+							luasnip = "[Snippet]",
+							buffer = "[Buffer]",
+							path = "[Path]",
+						})[entry.source.name]
+						return vim_item
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "orgmode" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "copilot" },
+					{ name = "codeium" },
+				},
+				confirm_opts = {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = false,
+				},
+				window = {
+					documentation = cmp.config.window.bordered(),
+				},
+				experimental = {
+					ghost_text = true,
+				},
+			}
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		event = "InsertEnter",
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
+				opts = function(_, opts)
+					local cmp = require("cmp")
+					local luasnip = require("luasnip")
+					opts.snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					}
+					opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					})
+					opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					})
+					table.insert(opts.sources, { name = "luasnip" })
+				end,
+			},
+		},
+	},
+}
