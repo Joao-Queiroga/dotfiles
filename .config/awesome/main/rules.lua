e115e574518ee4580ed86581a719f91a4ef4cb42
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-05-17 14:53:07 -0300

    awesome: change default browser to firefox

--- .config/awesome/main/rules.lua
+++ .config/awesome/main/rules.lua
@@ -67,7 +67,7 @@ function _M.get(clientkeys, clientbuttons)
 		-- { rule = { class = "Firefox" },
 		--   properties = { screen = 1, tag = "2" } },
 		{
-			rule = { class = "Firefox" },
+			rule = { class = "firefox" },
 			properties = { screen = 1, tag = "www" },
 		},
 		{
