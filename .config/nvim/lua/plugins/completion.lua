3a9899bd61db26f1d4178b8195786ea6aa42f83c
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-06-02 12:55:16 -0300

    nvim: correct cmp S-Tab function

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -135,7 +135,7 @@ return {
 					})
 					opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_next_item()
+							cmp.select_prev_item()
 						elseif luasnip.jumpable(-1) then
 							luasnip.jump(-1)
 						else
