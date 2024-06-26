828208d1768acb070662cdb9530d9a6188e3eb3d
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-06-26 19:34:13 -0300

    nvim: add flash.nvim plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -35,6 +35,22 @@ return {
 		opts = {},
 	},
 	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				search = { enabled = true },
+			},
+		},
+    -- stylua: ignore
+    keys = {
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+	},
+	{
 		"ahmedkhalf/project.nvim",
 		event = "VeryLazy",
 		dependencies = {
