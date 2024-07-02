291966fa6d8fadb8b474f3ddbeb395da8cfb965d
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-07-02 12:09:40 -0300

    nvim: add neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,51 @@
+return {
+	{
+		"rcasia/neotest-java",
+	},
+	{
+		"nvim-neotest/neotest",
+
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				["neotest-java"] = {},
+			},
+		},
+		config = function(_, opts)
+			if opts.adapters then
+				local adapters = {}
+				for name, config in pairs(opts.adapters or {}) do
+					if type(name) == "number" then
+						if type(config) == "string" then
+							config = require(config)
+						end
+						adapters[#adapters + 1] = config
+					elseif config ~= false then
+						local adapter = require(name)
+						if type(config) == "table" and not vim.tbl_isempty(config) then
+							local meta = getmetatable(adapter)
+							if adapter.setup then
+								adapter.setup(config)
+							elseif adapter.adapter then
+								adapter.adapter(config)
+								adapter = adapter.adapter
+							elseif meta and meta.__call then
+								adapter(config)
+							else
+								error("Adapter " .. name .. " does not support setup")
+							end
+						end
+						adapters[#adapters + 1] = adapter
+					end
+				end
+				opts.adapters = adapters
+			end
+			require("neotest").setup(opts)
+		end,
+	},
+}
