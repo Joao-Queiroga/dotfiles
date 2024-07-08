3a7e137dc3fd16fe3a0ca56e9db64ffd7ffd2e24
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-07-08 18:47:40 -0300

    ags: add app launcher

--- .config/ags/ts/main.ts
+++ .config/ags/ts/main.ts
@@ -1,10 +1,11 @@
 import { forMonitors } from "./utils.ts";
 import Bar from "./bar/bar.ts";
 import { compileCss, reapplyCss } from "./applycss";
+import { applauncher } from "./applauncher/applauncher";
 
 App.config({
   style: compileCss(),
-  windows: [...forMonitors(Bar)],
+  windows: [...forMonitors(Bar), applauncher],
 });
 
 const watchCss = Utils.monitorFile(`${App.configDir}/style`, (file, event) =>
