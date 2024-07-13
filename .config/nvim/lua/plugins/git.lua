9b24cbda37d1ee4b4d0a22d7ed0c5cb104a18f06
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-07-13 10:35:49 -0300

    nvim: add gitsigns-yadm.nvim plugin

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -3,18 +3,22 @@ return {
 		"Joao-Queiroga/lazygit-nvim",
 	},
 	{
+		"seanbreckenridge/gitsigns-yadm.nvim",
+		lazy = true,
+	},
+	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		opts = {
+			_on_attach_pre = function(_, callback)
+				require("gitsigns-yadm").yadm_signs(callback)
+			end,
 			signs = {
 				add = { text = "▎" },
 				change = { text = "▎" },
 				delete = { text = "󰐊" },
 				topdelete = { text = "󰐊" },
 				changedelete = { text = "▎" },
-			},
-			yadm = {
-				enable = true,
 			},
 		},
 	},
