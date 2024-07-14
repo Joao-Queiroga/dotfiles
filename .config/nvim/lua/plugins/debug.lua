37a0d3615ec62de850ba999a0c3c18ea050fc6a4
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-07-13 21:08:11 -0300

    nvim: configure dap keybindings and lazy-loading

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -2,29 +2,54 @@ local ensure_installed = {
 	"javadbg",
 	"javatest",
 }
+---@param config {args?:string[]|fun():string[]?}
+local function get_args(config)
+	local args = type(config.args) == "function" and (config.args() or {}) or config.args or {}
+	config = vim.deepcopy(config)
+	---@cast args string[]
+	config.args = function()
+		local new_args = vim.fn.input("Run with args: ", table.concat(args, " ")) --[[@as string]]
+		return vim.split(vim.fn.expand(new_args) --[[@as string]], " ")
+	end
+	return config
+end
 
 return {
 	{
 		"mfussenegger/nvim-dap",
+    -- stylua: ignore
 		keys = {
-			{ "<leader>d", "", desc = "Debug" },
+      { "<leader>d", "", desc = "+debug", mode = {"n", "v"} },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
+		dependencies = {
+			"jay-babu/mason-nvim-dap.nvim",
+			"rcarriga/nvim-dap-ui",
 			{
-				"<leader>db",
-				"<cmd>DapToggleBreakpoint<CR>",
-				mode = { "n", "v", "x" },
-				desc = "Toggle breakpoint",
-			},
-			{
-				"<leader>dc",
-				"<cmd>DapContinue<CR>",
-				mode = { "n", "v", "x" },
-				desc = "Continue",
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {},
 			},
 		},
-		event = "VeryLazy",
 	},
 	{
 		"jay-babu/mason-nvim-dap.nvim",
+		cmd = { "DapInstall", "DapUninstall" },
 		dependencies = {
 			"williamboman/mason.nvim",
 			"mfussenegger/nvim-dap",
@@ -35,20 +60,11 @@ return {
 	},
 	{
 		"rcarriga/nvim-dap-ui",
-		keys = {
-			{
-				"<leader>dt",
-				"<cmd>lua require('dapui').toggle()<CR>",
-				mode = { "n", "v", "x" },
-				desc = "Toggle UI",
-			},
-			{
-				"<leader>dr",
-				"<cmd>lua require('dapui').open({ reset = true })<CR>",
-				mode = { "n", "v", "x" },
-				desc = "Reset UI",
-			},
-		},
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+    },
 		event = "VeryLazy",
 		dependencies = {
 			"mfussenegger/nvim-dap",
