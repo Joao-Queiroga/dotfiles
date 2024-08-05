0f9c18ed4802717e15189dfea4ffd1ef56bf8862
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-08-05 14:47:56 -0300

    nvim: correct plugin name that changed

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -88,9 +88,9 @@ return {
 		end,
 	},
 	{
-		"MeanderingProgrammer/markdown.nvim",
+		"MeanderingProgrammer/render-markdown.nvim",
 		main = "render-markdown",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		ft = "markdown",
 		opts = {},
 	},
