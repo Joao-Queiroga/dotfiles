fcde87c97e704d83a234a9dd73ef3d9670fc26df
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-08-06 12:20:08 -0300

    fish: changed profile sourced

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-login
-  bass source ~/.profile
+  bass source ~/.config/shell/profile
   bass source /etc/profile
 end
 if status is-interactive
   fish_vi_key_bindings
-  # starship init fish | source
+  starship init fish | source
 end
