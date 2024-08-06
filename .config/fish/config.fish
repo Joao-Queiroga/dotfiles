f67d2799c7062355f5d478126a30adf5bea851d8
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-08-06 12:31:41 -0300

    fish: add zoxide and fzf integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,9 @@ if status is-login
   bass source /etc/profile
 end
 if status is-interactive
-  fish_vi_key_bindings
-  bass source ~/.config/shell/aliasesrc
   starship init fish | source
+  zoxide init fish | source
+  fzf --fish | source
+  bass source ~/.config/shell/aliasesrc
+  fish_vi_key_bindings
 end
