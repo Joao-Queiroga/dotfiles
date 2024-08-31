056fee85e08d9a3000bb295b85cc4e4a49eab849
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-08-31 15:35:27 -0300

    nvim: switch from codeium.nvim to neocodeium

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -79,14 +79,44 @@ return {
 		},
 	},
 	{
-		"nvim-cmp",
-		dependencies = {
+		"monkoose/neocodeium",
+		cmd = "Neocodium",
+		keys = {
 			{
-				"Exafunction/codeium.nvim",
-				cmd = "Codeium",
-				build = ":Codeium Auth",
-				opts = {},
+				"<c-g>",
+				function()
+					return require("neocodeium").accept()
+				end,
+				mode = "i",
+				expr = true,
 			},
+			{
+				"<c-;>",
+				function()
+					return require("neocodeium").cycle_or_complete(1)
+				end,
+				mode = "i",
+				expr = true,
+			},
+			{
+				"<c-,>",
+				function()
+					return require("neocodeium").cycle(-1)
+				end,
+				mode = "i",
+				expr = true,
+			},
+			{
+				"<c-x>",
+				function()
+					return require("neocodeium").clear()
+				end,
+				mode = "i",
+				expr = true,
+			},
+		},
+		opts = {
+			manual = true,
 		},
 	},
 }
