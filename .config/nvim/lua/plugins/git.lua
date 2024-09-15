241cb7f5aeca50edf744dd638bd61c1e15d6e9ec
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-09-15 09:05:33 -0300

    nvim: add conditional to yadm gitsigns to look for yadm first

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -31,14 +31,20 @@ return {
 	{
 		"seanbreckenridge/gitsigns-yadm.nvim",
 		lazy = true,
+		dependencies = {
+			"lewis6991/gitsigns.nvim",
+			opts = {
+				_on_attach_pre = function(_, callback)
+					require("gitsigns-yadm").yadm_signs(callback)
+				end,
+			},
+		},
+		enabled = vim.fn.executable("yadm") == 1,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		opts = {
-			_on_attach_pre = function(_, callback)
-				require("gitsigns-yadm").yadm_signs(callback)
-			end,
 			signs = {
 				add = { text = "▎" },
 				change = { text = "▎" },
