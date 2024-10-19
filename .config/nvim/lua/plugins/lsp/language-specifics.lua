046e3c1079c1cc20e138c9fef891f25863bc6e18
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-10-19 09:52:39 -0300

    nvim: made crates.nvim use in-process lsp

--- .config/nvim/lua/plugins/lsp/language-specifics.lua
+++ .config/nvim/lua/plugins/lsp/language-specifics.lua
@@ -61,20 +61,13 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
-		init = function()
-			vim.api.nvim_create_autocmd("BufRead", {
-				group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-				pattern = "Cargo.toml",
-				callback = function()
-					require("cmp").setup.buffer({ sources = { { name = "crates" } } })
-				end,
-			})
-		end,
 		opts = {
-			src = {
-				cmp = {
-					enabled = true,
-				},
+			lsp = {
+				enabled = true,
+				on_attach = handlers.on_attach,
+				actions = true,
+				completion = true,
+				hover = true,
 			},
 		},
 	},
