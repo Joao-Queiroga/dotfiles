a28a93fadadccff7eb6cddc442a85354155f4fb2
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-10-19 09:58:05 -0300

    nvim: remove lazydev nvim-cmp dependency

--- .config/nvim/lua/plugins/lsp/language-specifics.lua
+++ .config/nvim/lua/plugins/lsp/language-specifics.lua
@@ -13,18 +13,6 @@ return {
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
 			},
 		},
-		dependencies = {
-			{
-				"nvim-cmp",
-				opts = function(_, opts)
-					opts.sources = opts.sources or {}
-					table.insert(opts.sources, {
-						name = "lazydev",
-						group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-					})
-				end,
-			},
-		},
 	},
 	{ "Bilal2453/luvit-meta", lazy = true },
 	{ "justinsgithub/wezterm-types", lazy = true },
