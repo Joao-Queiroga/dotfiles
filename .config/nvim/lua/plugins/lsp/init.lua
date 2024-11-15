cb776a3bbab521ec37025101e961866f47748a20
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-11-15 13:09:22 -0300

    nvim: remove nil_ls auto-install

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,82 +1,81 @@
 local handlers = require("plugins.lsp.handlers")
 local installed_servers = {
-	"lua_ls",
-	"rust_analyzer",
-	"clangd",
-	"gopls",
-	"emmet_language_server",
-	"ts_ls",
-	"cssls",
-	"vimls",
-	"jsonls",
-	"yamlls",
-	"rnix",
-	"nil_ls",
+  "lua_ls",
+  "rust_analyzer",
+  "clangd",
+  "gopls",
+  "emmet_language_server",
+  "ts_ls",
+  "cssls",
+  "vimls",
+  "jsonls",
+  "yamlls",
+  "rnix",
 }
 ---@type LazySpec[]
 return {
-	{
-		"neovim/nvim-lspconfig",
-		keys = { { "<leader>l", "", desc = "LSP", mode = { "n", "v", "x" } } },
-		lazy = false,
-		config = handlers.setup,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"folke/neoconf.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			ensure_installed = installed_servers,
-			handlers = { handlers.choose_handler },
-		},
-	},
-	{
-		"RRethy/vim-illuminate",
-		event = "VeryLazy",
-		opts = {},
-		config = function(_, opts)
-			require("illuminate").configure(opts)
-		end,
-	},
-	{
-		"folke/neoconf.nvim",
-		opts = {},
-	},
-	{
-		"kosayoda/nvim-lightbulb",
-		event = "LspAttach",
-		opts = {
-			autocmd = {
-				enabled = true,
-			},
-		},
-	},
-	{
-		"folke/trouble.nvim",
-		keys = {
-			{
-				"<leader>ld",
-				"<cmd>Trouble diagnostics toggle<cr>",
-				mode = { "n", "v", "x" },
-				desc = "Document Diagnostics",
-			},
-			{ "<leader>lq", "<cmd>Trouble quickfix toggle<cr>", mode = { "n", "v", "x" }, desc = "Quickfixes" },
-			{ "<leader>ll", "<cmd>Trouble loclist toggle<cr>", mode = { "n", "v", "x" }, desc = "Loclist" },
-			{
-				"<leader>lR",
-				"<cmd>Trouble lsp_references toggle<cr>",
-				mode = { "n", "v", "x" },
-				desc = "LSP References",
-			},
-		},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		cmd = "Trouble",
-		opts = {},
-	},
-	{ import = "plugins.lsp.navic" },
-	{ import = "plugins.lsp.language-specifics" },
+  {
+    "neovim/nvim-lspconfig",
+    keys = { { "<leader>l", "", desc = "LSP", mode = { "n", "v", "x" } } },
+    lazy = false,
+    config = handlers.setup,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "folke/neoconf.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      ensure_installed = installed_servers,
+      handlers = { handlers.choose_handler },
+    },
+  },
+  {
+    "RRethy/vim-illuminate",
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+    end,
+  },
+  {
+    "folke/neoconf.nvim",
+    opts = {},
+  },
+  {
+    "kosayoda/nvim-lightbulb",
+    event = "LspAttach",
+    opts = {
+      autocmd = {
+        enabled = true,
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    keys = {
+      {
+        "<leader>ld",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        mode = { "n", "v", "x" },
+        desc = "Document Diagnostics",
+      },
+      { "<leader>lq", "<cmd>Trouble quickfix toggle<cr>", mode = { "n", "v", "x" }, desc = "Quickfixes" },
+      { "<leader>ll", "<cmd>Trouble loclist toggle<cr>", mode = { "n", "v", "x" }, desc = "Loclist" },
+      {
+        "<leader>lR",
+        "<cmd>Trouble lsp_references toggle<cr>",
+        mode = { "n", "v", "x" },
+        desc = "LSP References",
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
+    opts = {},
+  },
+  { import = "plugins.lsp.navic" },
+  { import = "plugins.lsp.language-specifics" },
 }
