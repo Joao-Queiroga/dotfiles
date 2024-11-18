5d8da4d81f7d0966af4c32f454a83b733cea222a
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-11-18 14:11:59 -0300

    nvim: switched from dashboard.nvim to Snacks dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,65 +1,16 @@
----@type LazySpec[]
+---@type LazySpec
 return {
-	"nvimdev/dashboard-nvim",
-	lazy = false,
-	opts = {
-		theme = "doom",
-		config = {
-			week_header = {
-				enable = true,
-			},
-			center = {
-				{
-					action = "Telescope find_files",
-					desc = " Find File",
-					icon = " ",
-					key = "f",
-				},
-				{
-					action = "ene | startinsert",
-					desc = " New File",
-					icon = " ",
-					key = "n",
-				},
-				{
-					action = "Telescope oldfiles",
-					desc = " Recent Files",
-					icon = " ",
-					key = "r",
-				},
-				{
-					action = "Telescope live_grep",
-					desc = " Find Text",
-					icon = " ",
-					key = "g",
-				},
-				{
-					action = "e " .. vim.fn.stdpath("config") .. "/init.lua",
-					desc = " Config",
-					icon = " ",
-					key = "c",
-				},
-				{
-					action = 'lua require("persistence").load()',
-					desc = " Restore Session",
-					icon = " ",
-					key = "s",
-				},
-				{
-					action = "Lazy",
-					desc = " Lazy",
-					icon = "󰒲 ",
-					key = "l",
-				},
-				{
-					action = function()
-						vim.api.nvim_input("<cmd>qa<cr>")
-					end,
-					desc = " Quit",
-					icon = " ",
-					key = "q",
-				},
-			},
-		},
-	},
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
 }
