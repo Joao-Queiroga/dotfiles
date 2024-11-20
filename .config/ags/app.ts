3dc36d4e7bdcac8916187dc8623e0dcbf796f274
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-11-20 07:39:07 -0300

    ags: migrate AppLauncher

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk3";
 import style from "./style/style.scss";
 import Bar from "./widget/Bar";
+import Applauncher from "./widget/Applauncher";
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar);
+    Applauncher();
   },
 });
