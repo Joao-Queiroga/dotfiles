b66f571cd4e365bb97611db0c310cb0a0d141f77
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2024-12-03 10:26:10 -0300

    nvim: add hyprlang lsp to autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,32 +2,45 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- open .h files as C instead of C++
 autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.h",
-	callback = function()
-		vim.bo.filetype = "c"
-	end,
+  pattern = "*.h",
+  callback = function()
+    vim.bo.filetype = "c"
+  end,
 })
 
 -- Open files in zsh functions folder as zsh
 autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = vim.fn.expand("~/.config/zsh/functions/*"),
-	callback = function()
-		vim.bo.filetype = "zsh"
-	end,
+  pattern = vim.fn.expand("~/.config/zsh/functions/*"),
+  callback = function()
+    vim.bo.filetype = "zsh"
+  end,
 })
 
 -- Add neovim files to yadm on creation
 autocmd({ "BufNewFile" }, {
-	pattern = vim.fn.expand("~/.config/nvim/**", true, true),
-	callback = function()
-		os.execute("yadm add " .. vim.api.nvim_buf_get_name(0))
-	end,
+  pattern = vim.fn.expand("~/.config/nvim/**", true, true),
+  callback = function()
+    os.execute("yadm add " .. vim.api.nvim_buf_get_name(0))
+  end,
 })
 
 -- change cwd when editing a config file
 autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = vim.fn.expand("~/.config/*/*", true, true),
-	callback = function()
-		vim.cmd("cd " .. vim.fn.expand("%:p:h"))
-	end,
+  pattern = vim.fn.expand("~/.config/*/*", true, true),
+  callback = function()
+    vim.cmd("cd " .. vim.fn.expand("%:p:h"))
+  end,
+})
+
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.hl", "hypr*.conf" },
+  callback = function(event)
+    print(string.format("starting hyprls for %s", vim.inspect(event)))
+    vim.lsp.start({
+      name = "hyprlang",
+      cmd = { "hyprls" },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
 })
