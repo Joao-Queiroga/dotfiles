2234cf9c575e3d166df90072ec91dcb07311c8fe
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-01-02 19:30:44 -0300

    wezterm modify font configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono", { weight = "DemiBold" })
 config.font_size = 12
 config.force_reverse_video_cursor = true
 config.hide_mouse_cursor_when_typing = false
@@ -16,7 +16,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_padding = {
-	top = 0,
 	bottom = 0,
 	left = 0,
 	right = 0,
