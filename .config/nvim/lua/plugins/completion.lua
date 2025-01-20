ae792b118fb7a7aad91a471e2b722a25a90ad67f
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-01-19 21:26:37 -0300

    nvim: configure blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -18,14 +18,32 @@ return {
         ["<C-j>"] = { "scroll_documentation_down", "fallback" },
         ["<C-k>"] = { "scroll_documentation_up", "fallback" },
       },
-      signature = { enabled = true },
       appearance = {
         nerd_font_variant = "normal",
         kind_icons = vim.g.kind_icons,
       },
       completion = {
+        accept = {
+          -- experimental auto-brackets support
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          enabled = true,
+        },
         list = { selection = { preselect = false, auto_insert = true } },
       },
+      signature = { enabled = true },
     },
   },
 }
