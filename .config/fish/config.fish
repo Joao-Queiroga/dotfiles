b9ec00ceaefd9d439271c850159fefc539739190
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-02 17:25:20 -0300

    fish: correct alias cd to z

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,14 @@ if status is-login
   bass source /etc/profile
 end
 if status is-interactive
-  type -q starship; and starship init fish | source; enable_transience
-  type -q zoxide; and zoxide init fish | source
+  type -q starship; and starship init fish | source; and enable_transience
+  type -q zoxide; and zoxide init fish | source; and alias cd=z
   type -q atuin; and atuin init fish | source
   type -q fzf; and fzf --fish | source
-  bass source ~/.config/shell/aliasesrc
   fish_vi_key_bindings
 
-  set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
   mkdir -p ~/.config/fish/completions
   carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
   carapace _carapace | source
+  bass source ~/.config/shell/aliasesrc
 end
