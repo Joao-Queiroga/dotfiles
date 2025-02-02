83a27a3a43e8087e00afc0c99389c4f22436c978
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-02 16:51:50 -0300

    fish: add alias for lazygit yadm

---
+++ .config/fish/functions/ly.fish
@@ -0,0 +1,4 @@
+function ly --wraps='lazygit --git-dir=/home/joaoqueiroga/.local/share/yadm/repo.git --work-tree=/home/joaoqueiroga' --description 'alias ly=lazygit --git-dir=/home/joaoqueiroga/.local/share/yadm/repo.git --work-tree=/home/joaoqueiroga'
+  lazygit --git-dir=/home/joaoqueiroga/.local/share/yadm/repo.git --work-tree=/home/joaoqueiroga $argv
+        
+end
