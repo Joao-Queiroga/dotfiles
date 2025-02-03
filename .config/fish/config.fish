05c8f0cd8bf55d26b1015236db7aac80af3b82c0
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-03 17:23:44 -0300

    fish: removed carapace workarround for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,8 +9,6 @@ if status is-interactive
   type -q fzf; and fzf --fish | source
   fish_vi_key_bindings
 
-  mkdir -p ~/.config/fish/completions
-  carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
   carapace _carapace | source
   bass source ~/.config/shell/aliasesrc
 end
