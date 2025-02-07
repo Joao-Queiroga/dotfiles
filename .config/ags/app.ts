0dfa739aec808ec3e9fb4a790d7e374427baad3a
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-07 13:23:53 -0300

    ags: migrate applauncher to gtk4

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk4";
 import style from "./style/style.scss";
 import Bar from "./widget/Bar";
+import AppLauncher from "./widget/AppLauncher";
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar);
+    AppLauncher();
   },
 });
---
+++ .config/ags/widget/AppLauncher.tsx
@@ -0,0 +1,81 @@
+import { Variable } from "astal";
+import { App, Astal, Gdk, Gtk } from "astal/gtk4";
+import AstalApps from "gi://AstalApps";
+
+const hide = () => App.get_window("launcher")!.hide()
+
+const AppButton = ({ app }: { app: AstalApps.Application }) => (
+  <button
+    cssClasses={['AppButton']}
+    onClicked={() => { hide(); app.launch() }}>
+    <box>
+      <image icon_name={app.icon_name} />
+      <box valign={Gtk.Align.CENTER} vertical>
+        <label
+          cssClasses={['name']}
+          xalign={0}
+          label={app.name}
+        />
+        {app.description && <label
+          cssClasses={['description']}
+          wrap
+          xalign={0}
+          label={app.description}
+        />}
+      </box>
+    </box>
+  </button>
+)
+
+export default function AppLauncher() {
+  const apps = new AstalApps.Apps()
+
+  const text = Variable("")
+  const list = text(text => apps.fuzzy_query(text).slice(0, 8))
+  const onEnter = () => {
+    apps.fuzzy_query(text.get())?.[0].launch()
+    hide();
+  }
+
+  return <window
+    name="launcher"
+    exclusivity={Astal.Exclusivity.IGNORE}
+    visible={false}
+    keymode={Astal.Keymode.EXCLUSIVE}
+    application={App}
+    onShow={() => { apps.reload(); text.set("") }}
+    onKeyPressed={(_, keyval) => {
+      (keyval == Gdk.KEY_Escape) && hide()
+    }}>
+    <box widthRequest={500} heightRequest={500} cssClasses={['AppLauncher']} vertical>
+      <entry
+        placeholder_text="Search"
+        primaryIconName="system-search-symbolic"
+        onNotifyText={self => text.set(self.text)}
+        onActivate={onEnter}
+        setup={self =>
+          App.connect('window-toggled', () => {
+            if (App.get_window("launcher")?.visible) {
+              self.grab_focus()
+              self.text = ""
+            }
+          }
+          )
+        }
+      />
+      <box cssClasses={['AppList']} spacing={6} vertical visible={list.as(l => l.length > 0)}>
+        {list.as(list => list.map(app => (
+          <AppButton app={app} />
+        )))}
+      </box>
+      <box
+        halign={Gtk.Align.CENTER}
+        cssClasses={['not-found']}
+        vertical
+        visible={list.as(l => l.length === 0)}>
+        <image icon_name="system-search-symbolic" />
+        <label label="No match found" />
+      </box>
+    </box>
+  </window>
+}
