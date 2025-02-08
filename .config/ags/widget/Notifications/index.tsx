de563a4dff2119bf637645f0624e88da66a3127f
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-08 13:43:34 -0300

    ags: improved notifications code

--- .config/ags/widget/Notifications/index.tsx
+++ .config/ags/widget/Notifications/index.tsx
@@ -2,29 +2,18 @@ import { bind } from 'astal';
 import { App, Astal } from 'astal/gtk4';
 import { Notification } from './Notification';
 import { NotifiationMap } from './NotificationMap';
-const { TOP, RIGHT } = Astal.WindowAnchor;
 
 export const allNotifications = new NotifiationMap()
 export default function Notifications() {
-  let notif: Astal.Window;
+  const { TOP, RIGHT } = Astal.WindowAnchor;
   return <window
     name="notifications"
     anchor={TOP | RIGHT}
     application={App}
-    visible={false}
-    setup={(self) => notif = self}
+    visible={bind(allNotifications).as(n => n.length > 0)}
   >
     <box vertical>
-      {bind(allNotifications).as((n) => {
-        if (notif)
-          (n.length == 0)
-            ? notif.hide()
-            : notif.show()
-
-        return n.map(Notification)
-      }
-      )}
-
+      {bind(allNotifications).as((n) => n.map(Notification))}
     </box>
   </window>
 }
