3cefe9670ac64ef2de39cf1f6b3404b266839c4e
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-15 12:14:07 -0300

    nvim: add colorful-menu.nvim plugin

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -31,7 +31,16 @@ return {
         },
         menu = {
           draw = {
-            treesitter = { "lsp" },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
           },
         },
         documentation = {
@@ -45,5 +54,10 @@ return {
       },
       signature = { enabled = true },
     },
+  },
+  {
+    "xzbdmw/colorful-menu.nvim",
+    lazy = true,
+    opts = {},
   },
 }
