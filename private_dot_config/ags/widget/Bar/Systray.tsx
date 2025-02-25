6fd0ea3de1d11a1782fbb77514c087fb71f6cf99
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-02-25 12:45:05 -0300

    ags: add functionality to systray menu to activate on left click and open menu on right click

--- private_dot_config/ags/widget/Bar/Systray.tsx
+++ private_dot_config/ags/widget/Bar/Systray.tsx
@@ -1,18 +1,37 @@
 import { bind } from "astal";
-import { hook } from "astal/gtk4";
+import { Gdk, Gtk, hook } from "astal/gtk4";
 import AstalTray from "gi://AstalTray";
+
+const createMenu = (item: AstalTray.TrayItem): Gtk.PopoverMenu => {
+  const popovermenu = Gtk.PopoverMenu.new_from_model(item.menuModel)
+  popovermenu.insert_action_group("dbusmenu", item.actionGroup)
+
+  return popovermenu
+}
 
 export const Systray = () => {
   const tray = AstalTray.get_default()
 
   return <box cssClasses={['systray']}>
-    {bind(tray, 'items').as(items => items.map(item => (
-      <menubutton tooltip_markup={bind(item, 'tooltip_markup')}
+    {bind(tray, 'items').as(items => items.map(item => {
+      const menu = createMenu(item)
+      return <><button tooltip_markup={bind(item, 'tooltip_markup')}
         tooltipMarkup={bind(item, 'tooltip_markup')}
-        menuModel={bind(item, 'menuModel')}
-        setup={self => hook(self, item, "notify::action-group", () => self.insert_action_group("dbusmenu", item.action_group))}>
+        onButtonPressed={(_, event) => {
+          const button = event.get_button()
+          if (button == Gdk.BUTTON_PRIMARY)
+            item.activate(0, 0)
+          else if (button == Gdk.BUTTON_SECONDARY)
+            menu.popup()
+        }}
+        setup={_ => {
+          hook(menu, item, "notify::action-group", () => menu?.insert_action_group("dbusmenu", item.actionGroup))
+          hook(menu, item, "notify::menumodel", () => menu?.set_menu_model(item.menuModel))
+        }}>
         <image gicon={bind(item, 'gicon')} />
-      </menubutton>
-    )))}
+      </button>
+        {menu}
+      </>
+    }))}
   </box >
 }
