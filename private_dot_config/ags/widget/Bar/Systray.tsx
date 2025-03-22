fd0c94b5fc14a75959b4fea43f1de6a946840223
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-03-22 15:12:20 -0300

    ags: add hook to createMenu function

--- private_dot_config/ags/widget/Bar/Systray.tsx
+++ private_dot_config/ags/widget/Bar/Systray.tsx
@@ -2,9 +2,12 @@ import { bind } from "astal";
 import { Gdk, Gtk, hook } from "astal/gtk4";
 import AstalTray from "gi://AstalTray";
 
-const createMenu = (item: AstalTray.TrayItem): Gtk.PopoverMenu => {
+
+const createTrayMenu = (item: AstalTray.TrayItem): Gtk.PopoverMenu => {
   const popovermenu = Gtk.PopoverMenu.new_from_model(item.menuModel)
   popovermenu.insert_action_group("dbusmenu", item.actionGroup)
+  hook(popovermenu, item, "notify::action-group", () => popovermenu?.insert_action_group("dbusmenu", item.actionGroup))
+  hook(popovermenu, item, "notify::menumodel", () => popovermenu?.set_menu_model(item.menuModel))
 
   return popovermenu
 }
@@ -14,22 +17,19 @@ export const Systray = () => {
 
   return <box cssClasses={['systray']}>
     {bind(tray, 'items').as(items => items.map(item => {
-      const menu = createMenu(item)
-      return <box><button tooltip_markup={bind(item, 'tooltip_markup')}
-        tooltipMarkup={bind(item, 'tooltip_markup')}
-        onButtonPressed={(_, event) => {
-          const button = event.get_button()
-          if (button == Gdk.BUTTON_PRIMARY)
-            item.activate(0, 0)
-          else if (button == Gdk.BUTTON_SECONDARY)
-            menu.popup()
-        }}
-        setup={_ => {
-          hook(menu, item, "notify::action-group", () => menu?.insert_action_group("dbusmenu", item.actionGroup))
-          hook(menu, item, "notify::menumodel", () => menu?.set_menu_model(item.menuModel))
-        }}>
-        <image gicon={bind(item, 'gicon')} />
-      </button>
+      const menu = createTrayMenu(item)
+      return <box cssClasses={['tray-item']}>
+        <button tooltip_markup={bind(item, 'tooltip_markup')}
+          tooltipMarkup={bind(item, 'tooltip_markup')}
+          onButtonPressed={(_, event) => {
+            const button = event.get_button()
+            if (button == Gdk.BUTTON_PRIMARY)
+              item.activate(0, 0)
+            else if (button == Gdk.BUTTON_SECONDARY)
+              menu.popup()
+          }}>
+          <image gicon={bind(item, 'gicon')} />
+        </button>
         {menu}
       </box>
     }))}
