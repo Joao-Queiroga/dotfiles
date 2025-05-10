7843a78d32ec80c90cb2a48c0df80c0257c4ecd2
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-05-10 10:01:28 -0300

    ags: change PopoverMenu to tsx and modularize function

---
+++ .config/ags/lib/customWidgets/PopoverMenu.ts
@@ -0,0 +1,3 @@
+import { astalify, Gtk } from "astal/gtk4";
+
+export const PopoverMenu = astalify<Gtk.PopoverMenu, Gtk.PopoverMenu.ConstructorProps>(Gtk.PopoverMenu);
--- .config/ags/widget/Bar/Systray.tsx
+++ .config/ags/widget/Bar/Systray.tsx
@@ -1,45 +1,46 @@
 import { bind } from "astal";
-import { Gdk, Gtk, hook } from "astal/gtk4";
+import { Gdk, Gtk } from "astal/gtk4";
 import AstalTray from "gi://AstalTray";
+import { PopoverMenu } from "../../lib/customWidgets/PopoverMenu";
 
-const createTrayMenu = (item: AstalTray.TrayItem): Gtk.PopoverMenu => {
-  const popovermenu = Gtk.PopoverMenu.new_from_model(item.menuModel);
-  popovermenu.insert_action_group("dbusmenu", item.actionGroup);
-  hook(popovermenu, item, "notify::action-group", () => popovermenu?.insert_action_group("dbusmenu", item.actionGroup));
-  hook(popovermenu, item, "notify::menumodel", () => popovermenu?.set_menu_model(item.menuModel));
-
-  return popovermenu;
+const TrayItem = (item: AstalTray.TrayItem) => {
+  const menu = (
+    <PopoverMenu
+      flags={Gtk.PopoverMenuFlags.NESTED}
+      menuModel={bind(item, "menuModel")}
+      setup={self => {
+        bind(item, "actionGroup").as(ag => self.insert_action_group("dbusmenu", ag));
+      }}
+    />
+  ) as Gtk.PopoverMenu;
+  return (
+    <box cssClasses={["tray-item"]}>
+      <button
+        tooltipMarkup={bind(item, "tooltip_markup")}
+        onButtonPressed={(_, event) => {
+          switch (event.get_button()) {
+            case Gdk.BUTTON_PRIMARY:
+              if (item.isMenu) {
+                menu.popup();
+              } else {
+                const [_, x, y] = event.get_position();
+                item.activate(x, y);
+              }
+              break;
+            case Gdk.BUTTON_SECONDARY:
+              menu.popup();
+          }
+        }}
+      >
+        <image gicon={bind(item, "gicon")} />
+      </button>
+      {menu}
+    </box>
+  );
 };
 
 export const Systray = () => {
   const tray = AstalTray.get_default();
 
-  return (
-    <box cssClasses={["systray"]}>
-      {bind(tray, "items").as(items =>
-        items.map(item => {
-          const menu = createTrayMenu(item);
-          return (
-            <box cssClasses={["tray-item"]}>
-              <button
-                tooltipMarkup={bind(item, "tooltip_markup")}
-                onButtonPressed={(_, event) => {
-                  switch (event.get_button()) {
-                    case Gdk.BUTTON_PRIMARY:
-                      item.activate(0, 0);
-                      break;
-                    case Gdk.BUTTON_SECONDARY:
-                      menu.popup();
-                  }
-                }}
-              >
-                <image gicon={bind(item, "gicon")} />
-              </button>
-              {menu}
-            </box>
-          );
-        }),
-      )}
-    </box>
-  );
+  return <box cssClasses={["systray"]}>{bind(tray, "items").as(items => items.map(TrayItem))}</box>;
 };
