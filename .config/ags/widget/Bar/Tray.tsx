772bbc3c3f5906ae003274ed5546e5900e22bdbd
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-06-17 16:41:33 -0300

    ags: replace EventControllerLegacy with GestureClick in tray

--- .config/ags/widget/Bar/Tray.tsx
+++ .config/ags/widget/Bar/Tray.tsx
@@ -5,35 +5,31 @@ import Gio from "gi://Gio?version=2.0";
 
 const TrayItem = ({ item }: { item: AstalTray.TrayItem }) => (
   <menubutton class="tray-item" tooltip_markup={createBinding(item, "tooltipMarkup")}>
-    <Gtk.EventControllerLegacy
+    <Gtk.GestureClick
+      button={0}
       propagationPhase={Gtk.PropagationPhase.CAPTURE}
-      onEvent={(self, event) => {
+      onPressed={self => {
+        const button = self.get_current_button();
+        if (button === Gdk.BUTTON_SECONDARY || (item.isMenu && button === Gdk.BUTTON_PRIMARY)) {
+          item.about_to_show();
+        }
+        self.set_state(Gtk.EventSequenceState.CLAIMED);
+      }}
+      onReleased={(self, _, x, y) => {
         const menubutton = self.get_widget() as Gtk.MenuButton;
-
-        const type = event.get_event_type();
-        if (type === Gdk.EventType.BUTTON_PRESS) {
-          const button = (event as Gdk.ButtonEvent).get_button();
-          if (button === Gdk.BUTTON_SECONDARY || (item.is_menu && button === Gdk.BUTTON_PRIMARY)) {
-            item.about_to_show();
-          }
-        }
-        if (type === Gdk.EventType.BUTTON_RELEASE) {
-          const button = (event as Gdk.ButtonEvent).get_button();
-          const [_, x, y] = event.get_position();
-          if (event.get_surface() != menubutton.get_native()?.get_surface()) return;
-          if (button === Gdk.BUTTON_PRIMARY) {
-            if (item.isMenu) {
-              menubutton.popup();
-            } else {
-              item.activate(x, y);
-            }
-          } else if (button === Gdk.BUTTON_MIDDLE) {
-            item.secondary_activate(x, y);
-          } else {
+        const button = self.get_current_button();
+        if (button === Gdk.BUTTON_PRIMARY) {
+          if (item.isMenu) {
             menubutton.popup();
+          } else {
+            item.activate(x, y);
           }
+        } else if (button === Gdk.BUTTON_MIDDLE) {
+          item.secondary_activate(x, y);
+        } else {
+          menubutton.popup();
         }
-        return true;
+        self.set_state(Gtk.EventSequenceState.CLAIMED);
       }}
     />
     <image class="tray-icon" gicon={createBinding(item, "gicon")} />
