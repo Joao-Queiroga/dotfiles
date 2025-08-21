0a788acea43aa816e0def218132f5d96327f6bfc
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-08-20 21:30:06 -0300

    delete unecessary configs

--- .config/ags2/lib/customWidgets/PopoverMenu.ts
+++
@@ -1,3 +0,0 @@
-import { astalify, Gtk } from "astal/gtk4";
-
-export const PopoverMenu = astalify<Gtk.PopoverMenu, Gtk.PopoverMenu.ConstructorProps>(Gtk.PopoverMenu);
--- .config/ags2/lib/util.ts
+++
@@ -1,3 +0,0 @@
-export function range(length: number, start = 1) {
-  return Array.from({ length }, (_, i) => i + start);
-}
