b6e15da84dc1c94f43034fa7842fa4d406671938
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-04 10:56:38 -0300

    home-manager: add variables definition

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -10,6 +10,12 @@
 
   home.stateVersion = "25.05";
 
+  home.sessionPath = [
+    "${config.home.sessionVariables.CARGO_HOME}/bin"
+    "${config.home.sessionVariables.GOPATH}/bin"
+    "$HOME/.local/bin"
+  ];
+
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     dust
@@ -118,7 +124,37 @@
     extraConfig = "cursor none";
   };
 
-  home.sessionVariables = { EDITOR = "nvim"; };
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PF_INFO = "ascii title os host kernel uptime pkgs wm memory palette";
+
+    MESA_SHADER_CACHE_DIR = "$HOME/.cache/mesa_shader_cache";
+    MESA_SHADER_CACHE_MAX_SIZE = "12G";
+    RADV_PERFTEST = "aco";
+
+    XINITRC = "${config.xdg.configHome}/x11/xinitrc";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
+    GOPATH = "${config.xdg.dataHome}/go";
+    CARGO_HOME = "${config.xdg.dataHome}/cargo";
+    RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+    WGETRC = "${config.xdg.configHome}/wget/wgetrc";
+    GNUPGHOME = "${config.xdg.dataHome}/gnupg";
+    ANDROID_SDK_HOME = "${config.xdg.configHome}/android";
+    ANDROID_USER_HOME = "${config.xdg.dataHome}/android";
+    BUN_INSTALL = "${config.xdg.dataHome}/bun";
+    NPM_CONFIG_INIT_MODULE = "${config.xdg.configHome}/npm/config/npm-init.js";
+    NPM_CONFIG_CACHE = "${config.xdg.cacheHome}/npm";
+    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+    WINEPREFIX = "${config.xdg.dataHome}/wine";
+
+    CARAPACE_HIDDEN = 1;
+    CARAPACE_LENIENT = 1;
+    CARAPACE_MATCH = 1;
+
+    W3M_DIR = "${config.xdg.configHome}/w3m";
+
+    MANPAGER = "nvim +Man!";
+  };
 
   xdg.configFile."uwsm/env".source =
     "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
