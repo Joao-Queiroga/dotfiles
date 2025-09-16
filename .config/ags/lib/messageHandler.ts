1882736cffd637c3642c0fe5be41019acbd45796
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-16 12:03:59 -0300

    ags: correct requestHandler function

--- .config/ags/lib/messageHandler.ts
+++ .config/ags/lib/messageHandler.ts
@@ -1,11 +1,12 @@
 import { dismissAllNotifications, dismissLastNotification } from "../widget/Notifications";
 
-export const requestHandler = (request: string, res: (res: any) => void) => {
-  if (request === "clearLastNotification") {
+export const requestHandler = (argv: string[], res: (res: any) => void) => {
+  const [cmd, arg, ...rest] = argv;
+  if (cmd === "clearLastNotification") {
     dismissLastNotification();
     res("ok");
   }
-  if (request === "clearAllNotifications") {
+  if (cmd === "clearAllNotifications") {
     dismissAllNotifications();
     res("ok");
   }
