077e89b5302811182f7b436e0e0248411468d718
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-09-29 12:18:04 -0300

    move nvim old config to create new one

--- .config/oldnvim/lua/plugins/dashboard.lua
+++ .config/oldnvim/lua/plugins/dashboard.lua
@@ -6,11 +6,7 @@ return {
   ---@type snacks.Config
   opts = {
     dashboard = {
-      sections = {
-        { section = "header" },
-        { section = "keys", gap = 1, padding = 1 },
-        { section = "startup" },
-      },
+      enable = true
     },
   },
 }
