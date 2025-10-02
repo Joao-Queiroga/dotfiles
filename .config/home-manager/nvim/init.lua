0e2821b31fd9fbfc0927448a148e2eadb7d9616b
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-10-01 21:22:13 -0300

    nvim: add autocmds

--- .config/home-manager/nvim/init.lua
+++ .config/home-manager/nvim/init.lua
@@ -1,14 +1,15 @@
 if vim.env.PROF then
-    -- vim.cmd.packadd('snacks.nvim')
-    require("snacks.profiler").startup({
-        startup = {
-            event = "UIEnter",
-        },
-    })
+  -- vim.cmd.packadd('snacks.nvim')
+  require("snacks.profiler").startup({
+    startup = {
+      event = "UIEnter",
+    },
+  })
 end
 vim.loader.enable()
 
 vim.cmd.colorscheme("tokyonight-night")
 require("config.options")
 require("config.keybindings")
+require("config.autocmd")
 require("plugins")
