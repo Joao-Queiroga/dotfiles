5463e45530fc288f532e89131ca3dfb355790443
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-10-05 23:40:59 -0300

    nvim: correct jdtls cmd

--- .config/home-manager/nvim/ftplugin/java.lua
+++ .config/home-manager/nvim/ftplugin/java.lua
@@ -1,8 +1,5 @@
 local config = {
-  cmd = {
-    vim.fn.exepath("jdtls"),
-    string.format("--jvm-arg=-javaagent:%s", vim.fn.expand("$MASON/share/jdtls/lombok.jar")),
-  },
+  cmd = { "jdtls" },
 
   settings = {
     java = {
