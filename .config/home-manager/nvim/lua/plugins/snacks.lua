401eae5d36b6becd35abc12f267008a800150de5
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-11-15 11:37:05 -0300

    nvim: configure snacks scope

--- .config/home-manager/nvim/lua/plugins/snacks.lua
+++ .config/home-manager/nvim/lua/plugins/snacks.lua
@@ -1,4 +1,4 @@
----@type PluginList
+---@type PluginSpec
 return {
   "snacks.nvim",
   lazy = false,
@@ -8,7 +8,8 @@ return {
       notifier = { enabled = true },
       quickfile = { enabled = true },
       words = { enabled = true },
-      indent = { char = "│" },
+      indent = { enabled = true },
+      scope = { enabled = true },
       image = { enabled = true },
       input = { enabled = true },
       picker = {
@@ -358,13 +359,56 @@ return {
       end,
       desc = "Colorschemes",
     },
-    --[[ -- LSP
-    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
-    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" }, ]]
+    -- LSP
+    {
+      "gd",
+      function()
+        Snacks.picker.lsp_definitions()
+      end,
+      desc = "Goto Definition",
+    },
+    {
+      "gD",
+      function()
+        Snacks.picker.lsp_declarations()
+      end,
+      desc = "Goto Declaration",
+    },
+    {
+      "gr",
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = "References",
+    },
+    {
+      "gI",
+      function()
+        Snacks.picker.lsp_implementations()
+      end,
+      desc = "Goto Implementation",
+    },
+    {
+      "gy",
+      function()
+        Snacks.picker.lsp_type_definitions()
+      end,
+      desc = "Goto T[y]pe Definition",
+    },
+    {
+      "<leader>ss",
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = "LSP Symbols",
+    },
+    {
+      "<leader>sS",
+      function()
+        Snacks.picker.lsp_workspace_symbols()
+      end,
+      desc = "LSP Workspace Symbols",
+    },
   },
 }
