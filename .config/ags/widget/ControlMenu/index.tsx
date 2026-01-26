8e24a1c0cdcca2a3b048fcab9f2fb9f2e7e65bad
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2026-01-26 10:34:44 -0300

    ags: hide control menu when clicking power button

--- .config/ags/widget/ControlMenu/index.tsx
+++ .config/ags/widget/ControlMenu/index.tsx
@@ -4,20 +4,29 @@ import { MicrophoneSlider, VolumeSlider } from "../../components/Volume";
 import { BrightnessSlider } from "../../components/Brightness";
 import { Buttons } from "../../components/PowerProfiles";
 
-export const ControlMenu = () => (
-  <popover hasArrow class="control-menu">
-    <box orientation={Gtk.Orientation.VERTICAL}>
-      <VolumeSlider />
-      <MicrophoneSlider />
-      <BrightnessSlider />
-      <centerbox>
-        <box $type="start" halign={Gtk.Align.START}>
-          <Buttons />
-        </box>
-        <button $type="end" onClicked={() => app.get_window("powermenu")!.show()}>
-          <image iconName="system-shutdown-symbolic" />
-        </button>
-      </centerbox>
-    </box>
-  </popover>
-);
+export const ControlMenu = () => {
+  let popover: Gtk.Popover;
+  return (
+    <popover $={self => (popover = self)} hasArrow class="control-menu">
+      <box orientation={Gtk.Orientation.VERTICAL}>
+        <VolumeSlider />
+        <MicrophoneSlider />
+        <BrightnessSlider />
+        <centerbox>
+          <box $type="start" halign={Gtk.Align.START}>
+            <Buttons />
+          </box>
+          <button
+            $type="end"
+            onClicked={() => {
+              popover.hide();
+              app.get_window("powermenu")!.show();
+            }}
+          >
+            <image iconName="system-shutdown-symbolic" />
+          </button>
+        </centerbox>
+      </box>
+    </popover>
+  );
+};
